950cc3a2876aae0950e43d0507cbf78fbbe36a4d
Frances Caldwell <64825979+FrancesCaldwell@users.noreply.github.com>
2023-08-13 22:53:47 -0700

    Update and rename countdown.html to countdown.php

--- travelup/countdown.php
+++ travelup/countdown.php
@@ -10,14 +10,14 @@
   <body>
     <!-- Navbar -->
     <div id="navbar">
-      <a href="travelup.html"
+      <a href="travelup.php"
         ><img src="travelup.png" style="width: 110px; height: 40px;"/ ></a
       >
     </div>
 
     <header id="landing-bg">
       <div class="center-countdown">
-        <a href="countdown.html" class="time-display" id="countdown-timer"></a>
+        <a href="countdown.php" class="time-display" id="countdown-timer"></a>
         <p id="current-ship"></p>
       </div>
     </header>
